51a439e8d849aaed99cc05a97a940f0c66613e32
Thiago Souza Xavier <thiago.xavier@localiza.com>
2023-10-04 15:58:19 -0300

    initial

---
+++ app/db/script.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS fiap;
+USE fiap;
+
+CREATE TABLE IF NOT EXISTS products (
+  id INT(11) AUTO_INCREMENT,
+  name VARCHAR(255),
+  price DECIMAL(10, 2),
+  PRIMARY KEY (id)
+);
+
+INSERT INTO products VALUE(0, 'Curso 1', 2500);
+INSERT INTO products VALUE(0, 'Curso 2', 900);
